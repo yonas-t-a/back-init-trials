abb36ea2068b8df783509e866401931e50f6c77b
yonas-t-a <yonas.tessema@aait.edu.et>
2025-04-17 15:12:35 +0300

    Final userRoute

--- back-end/routers/api/userRoute.js
+++ back-end/routers/api/userRoute.js
@@ -1,21 +1,20 @@
 import express from 'express';
-
 import {
-  getUser,
-  getUserById,
-  createUser,
-  updateUser,
-  deleteUser
+    getUser, 
+    getUserById, 
+    createUser, 
+    updateUser, 
+    deleteUser
 } from '../../controller/userController.js';
+
+import { authenticate, authorizeRoles } from '../../middleware/authMiddleware.js';
 
 const router = express.Router();
 
-router.route('/')
-    .get(getUser)
-    .post(createUser);
-router.route('/:id')
-    .get(getUserById)
-    .put(updateUser)
-    .delete(deleteUser);
+router.get('/', authenticate, authorizeRoles('admin'), getUser);
+router.get('/:id', authenticate, getUserById);
+router.post('/', authenticate , createUser);
+router.put('/:id', authenticate , updateUser);
+router.delete('/:id', authenticate, authorizeRoles('admin'), deleteUser);
 
 export default router;
